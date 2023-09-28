4db8d875fc8ea1f62569f8c1144c114e3b32c45b
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-28 15:44:31 +0200

    show all countries

--- src/main/java/org/java/Main.java
+++ src/main/java/org/java/Main.java
@@ -2,6 +2,10 @@ package org.java;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,9 +14,25 @@ public class Main {
 		final String user = "root";
 		final String password = "root";
 		
-		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
 			
-			System.out.println("Connessione stabilita correttamente");
+			PreparedStatement ps = con.prepareStatement("select c.country_id, c.name, r.name, c2.name"
+					+ " from countries c"
+					+ " join regions r on c.region_id = r.region_id"
+					+ " join continents c2 on c2.continent_id = r.continent_id"
+					+ " order by c.name ");
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				int id = rs.getInt("c.country_id");
+				String country_name = rs.getString("c.name");
+				String region_name = rs.getString("r.name");
+				String continent_name = rs.getString("c2.name");
+				
+				System.out.println(id + " |" + country_name + " | "  + region_name + " | "  + continent_name);
+				System.out.println("----------------------------------------------------------------");
+			}
 		} catch (Exception e) {
 			
 			System.out.println("Errore di connessione: " + e.getMessage());
